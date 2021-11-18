b8497fae3a084c8a0b04533cb9f584611e4d4861
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2021-11-18 17:31:10 -0300

    feat: adicionou requisito 3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Home from './Pages/Home';
+import ShoppingCart from './Pages/ShoppingCart';
 
 function App() {
   return (
     <BrowserRouter>
       <Route exact path="/" component={ Home } />
+      <Route exact path="/shoppingcart" component={ ShoppingCart } />
     </BrowserRouter>
   );
 }
