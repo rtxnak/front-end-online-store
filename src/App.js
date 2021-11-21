1f7122acffb65140f15488986a2586d2252cf3f7
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2021-11-21 20:02:07 -0300

    feat: adicionou requisito 7

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Home from './Pages/Home';
 import ShoppingCart from './Pages/ShoppingCart';
+import ProductDetails from './Pages/ProductsDetails';
 
 function App() {
   return (
     <BrowserRouter>
       <Route exact path="/" component={ Home } />
       <Route exact path="/shoppingcart" component={ ShoppingCart } />
+      <Route
+        exact
+        path="/productsdetails/:id"
+        render={ (props) => <ProductDetails { ...props } /> }
+        // render={
+        //   () => (<ProductDetails
+        //   />)
+        // }
+      />
     </BrowserRouter>
   );
 }
--- src/Components/CardP.js
+++ src/Components/CardP.js
@@ -1,22 +1,32 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class CardP extends Component {
   render() {
     const { arrProduct } = this.props;
+    const { id, title, thumbnail, price } = arrProduct;
     return (
       <div data-testid="product">
-        <span>
-          { arrProduct.title }
-        </span>
-        <img
-          src={ arrProduct.thumbnail }
-          alt={ arrProduct.title }
-          width="30px"
-        />
-        <span>
-          { arrProduct.original_price }
-        </span>
+        <Link
+          data-testid="product-detail-link"
+          to={ {
+            pathname: `/productsdetails/${id}`,
+            state: { arrProduct },
+          } }
+        >
+          <span>
+            { title }
+          </span>
+          <img
+            src={ thumbnail }
+            alt={ title }
+            width="30px"
+          />
+          <span>
+            { price }
+          </span>
+        </Link>
       </div>
     );
   }
@@ -27,3 +37,5 @@ CardP.propTypes = {
 };
 
 export default CardP;
+
+// transferir informação por rota dinamica =>https://www.youtube.com/watch?v=RUFxmAjbNbg
